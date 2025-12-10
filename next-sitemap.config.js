14fb25d5d4bdbb8aced179acdb8c1addb9d382ec
Guillermo <guillermo.sotelo@volvocars.com>
2025-12-10 10:07:35 +0100

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://www.anechooftheheart.com', // your domain
+  generateRobotsTxt: true, // generate robots.txt file
+  // Optionally exclude pages
+  exclude: ['/login'],
+};
