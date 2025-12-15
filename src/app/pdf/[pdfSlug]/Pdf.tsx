b42335ca23971c47f0a7b848837cac01e67a8b48
Guillermo <guillermo.sotelo@volvocars.com>
2025-12-15 21:29:00 +0100

    pdf

--- src/app/pdf/[pdfSlug]/Pdf.tsx
+++ src/app/pdf/[pdfSlug]/Pdf.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useContext, useEffect, useState } from 'react'
 import { capitalizeFirstLetter } from 'src/helpers'
 import { Document, Page, pdfjs } from 'react-pdf';
